a82c326bc9afe5b3514e7917b3ff047ee3851228
JW <walaro@kth.se>
2016-11-11 14:22:05 +0100

    Fixed migrations

--- articles/models.py
+++ articles/models.py
@@ -3,9 +3,9 @@ from author.models import AuthorProfile
 
 
 class Article(models.Model):
-	article_headline = models.CharField()
+	article_headline = models.CharField(max_length=256)
 	article_ingress = models.TextField()
-	html_article_content = models.CharField(default="")
+	html_article_content = models.TextField(default="")
 	date_published = models.DateTimeField()
 
 
@@ -14,10 +14,11 @@ class ArticleAuthor(models.Model):
 	author = models.ForeignKey(AuthorProfile)
 
 
+class Tag(models.Model):
+	tag = models.CharField(max_length=256)
+
 class ArticleTag(models.Model):
 	tag = models.ForeignKey(Tag)
 	article  = models.ForeignKey(Article)
 
 
-class Tag(models.Model):
-	tag = models.CharField()
--- author/models.py
+++ author/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class AuthorProfile(models.Model)
+class AuthorProfile(models.Model):
     author = models.ForeignKey(User)
     signature = models.CharField(max_length=256)
 
