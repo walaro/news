2ce5562102d323dc5feccf14af57e4d6318b0c42
Andreas Pålsson <apals@kth.se>
2016-11-11 14:14:52 +0100

    Add some models

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from author.models import AuthorProfile
 
 # Create your models here.
+All tables below have an id integer field which is the primary key. 
+
+
+class Article(models.Model):
+	article_headline = models.CharField()
+	article_ingress = models.TextField()
+	html_article_content = models.CharField(default="")
+	date_published = models.DateTimeField()
+
+
+class ArticleAuthor(models.Model):
+	article = models.ForeignKey(Article)
+	author = models.ForeignKey(AuthorProfile)
+
+
+class ArticleTag(models.Model):
+	tag = models.ForeignKey(Tag)
+	article  = models.ForeignKey(Article)
+
+
+
+class Tag(models.Model):
+	tag = models.CharField()
