39422b2852c3313b478aa9916321c5d8670261f0
JW <walaro@kth.se>
2016-11-11 14:27:27 +0100

    Added articles to admin

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Article, ArticleAuthor, Tag, ArticleTag
 
+admin.site.register(Article)
+admin.site.register(ArticleAuthor)
+admin.site.register(Tag)
+admin.site.register(ArticleTag)
 # Register your models here.
