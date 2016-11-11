6a4304f03dd71a718e42b920b88a62db2358a55d
JW <walaro@kth.se>
2016-11-11 14:35:51 +0100

    Collected static files

--- news/settings.py
+++ news/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-
+from os import path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -118,5 +118,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATIC_ROOT = path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (path.join(BASE_DIR, "news_static"),)
