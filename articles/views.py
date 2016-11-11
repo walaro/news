b74a006b6f447e989137d5c4cdc5329e5f525678
Andreas Pålsson <apals@kth.se>
2016-11-11 15:10:23 +0100

    Add view

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Article
+
+def index(request):
+	articles = Article.objects.order_by('-date_published')[:5]
+	context = {'articles': articles}
+	return render(request, 'articles/index.html', context)
