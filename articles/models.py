5db88ccd6531407739f1349bedec1a741433f228
JW <walaro@kth.se>
2016-12-08 14:27:38 +0100

    PENIS

--- articles/models.py
+++ articles/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from author.models import AuthorProfile
 
+class Tag(models.Model):
+	tag = models.CharField(max_length=256)
+
+	def __str__(self):
+		return self.tag
 
 class Article(models.Model):
 	article_headline = models.CharField(max_length=256)
 	article_ingress = models.TextField()
 	html_article_content = models.TextField(default="")
 	date_published = models.DateTimeField()
+	tags = models.ManyToManyField(Tag)
 
 	def __str__(self):
 		return self.article_headline
@@ -17,11 +23,6 @@ class ArticleAuthor(models.Model):
 	author = models.ForeignKey(AuthorProfile)
 
 
-class Tag(models.Model):
-	tag = models.CharField(max_length=256)
-
-	def __str__(self):
-		return self.tag
 
 class ArticleTag(models.Model):
 	tag = models.ForeignKey(Tag)
--- articles/urls.py
+++ articles/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='articles'),
     url(r'^(?P<pk>\d+)/edit$', views.edit_article, name='edit_article'),
-    url(r'(?P<pk>\d+)', views.view_article),
+    url(r'^(?P<pk>\d+)', views.view_article),
 ]
