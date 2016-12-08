0ba41d61d3dfca3f753a397d7fb8c6be6022077b
Andreas Pålsson <apals@kth.se>
2016-12-08 11:31:58 +0100

    try serializing django objects to xml

--- articles/views.py
+++ articles/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Article
+from django.core import serializers
 
 def index(request):
 	articles = Article.objects.order_by('-date_published')[:5]
 	context = {'articles': articles}
 	response = HttpResponse(content_type="application/xml; charset=utf-8")
 	f = open('news_static/test.xml', encoding="utf-8")
-	for line in f:
-		response.write(line)
+	data = serializers.serialize("xml", articles)
+
+	data = "<?xml-stylesheet type=\"text/xsl\" href=\"/static/articles.xsl\"?>" + data
+	response.write(data)
+	print(data)
 	f.close()
 	return response
