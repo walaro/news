6ebf6e9725f9fc25f9c9c9336e0ca616e3d27ec1
JW <walaro@kth.se>
2016-12-13 18:45:08 +0100

    Minimal login at /login and /logout

--- news/settings.py
+++ news/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
